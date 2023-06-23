ab558ee44c1ea631c55c45e0f437dd816af3af45
MartinSosa7 <martinchoelias7@gmail.com>
2023-06-23 16:38:22 -0300

    creacion component creacion-noticia

--- proyecto-final-frontend/src/app/app.module.ts
+++ proyecto-final-frontend/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { EditorModule } from '@tinymce/tinymce-angular';
+import { CreacionNoticiaComponent } from './components/creacion-noticia/creacion-noticia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreacionNoticiaComponent
   ],
   imports: [
     BrowserModule,
---
+++ proyecto-final-frontend/src/app/components/creacion-noticia/creacion-noticia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-creacion-noticia',
+  templateUrl: './creacion-noticia.component.html',
+  styleUrls: ['./creacion-noticia.component.css']
+})
+export class CreacionNoticiaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
