455696ffe1bc2d7fcb08ccc1031067205f3220f8
MartinSosa7 <martinchoelias7@gmail.com>
2023-06-23 04:34:05 -0300

    creacion del backend del proyecto

---
+++ proyecto-final-backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const { mongoose } = require('./database');
+var app = express();
+//middlewares
+app.use(express.json());
+app.use(cors({ origin: 'http://localhost:4200' }));
+//Cargamos el modulo de direccionamiento de rutas
+
+//app.use('/api/agente', require('./routes/agente.route.js'));
+
+//setting
+app.set('port', process.env.PORT || 3000);
+//starting the server
+app.listen(app.get('port'), () => {
+    console.log(`Server started on port`, app.get('port'));
+});
