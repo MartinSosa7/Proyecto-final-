c4751f8aaaaf99c15b2d6e45330fb8118784e1c9
MartinSosa7 <martinchoelias7@gmail.com>
2023-06-24 04:31:32 -0300

    codigo para resolver el problema del display por pantalla y tablas

--- proyecto-final-frontend/src/app/components/creacion-noticia/creacion-noticia.component.ts
+++ proyecto-final-frontend/src/app/components/creacion-noticia/creacion-noticia.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-creacion-noticia',
+  selector: 'creacion-noticia',
   templateUrl: './creacion-noticia.component.html',
   styleUrls: ['./creacion-noticia.component.css']
 })
 export class CreacionNoticiaComponent implements OnInit {
 
-  constructor() { }
+  Contenido = '';
+
+  constructor(private sanitizer: DomSanitizer, private elementRef: ElementRef) {
+    this.Contenido = '';
+   }
 
   ngOnInit(): void {
+  }
+
+  contenido(){
+    
+    const tempElement = document.createElement('div');
+    tempElement.innerHTML = this.Contenido;
+
+    const tableElement: HTMLElement = tempElement.querySelector('table')!;
+
+    if (tableElement) {
+      const clonedTableElement = tableElement.cloneNode(true) as HTMLElement;
+      if (tableElement.parentNode) {
+        tableElement.parentNode.replaceChild(clonedTableElement, tableElement);
+      }
+
+      clonedTableElement.classList.add('table', 'table-bordered', 'table-striped');
+    }
+
+    // Get the modified HTML string
+    this.Contenido = tempElement.innerHTML;
+    console.log(this.Contenido);
+  }
+
+  sanitizeHtml(html: string): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(html);
   }
 
 }
