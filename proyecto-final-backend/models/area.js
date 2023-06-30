1980c9ce3521f1f7e75685e90ba2a40c6a35728d
Antonela Flores <totte_30_98@hotmail.com>
2023-06-29 21:48:42 -0300

    detalles modificados

--- proyecto-final-backend/models/area.js
+++ proyecto-final-backend/models/area.js
@@ -4,7 +4,7 @@ const {Schema} = mongoose;
 const AreaSchema = new Schema({
  nombreArea: {type: String, required: true},
  tipo: {type:String, required:true},
- responsables: [{type: mongoose.Types.ObjectId, ref: 'Persona'}]
+ responsables: [{type: mongoose.Types.ObjectId, ref: 'Persona', required:true}]
 })
 
 module.exports = mongoose.models.Area || mongoose.model('Area', AreaSchema);
