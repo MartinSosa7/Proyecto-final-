72cbfded0439036db7d1949a2537cb42f8c8539b
EAES2S09\Cintia <cintiazamudio35@gmail.com>
2023-07-06 20:38:12 -0300

    -

--- proyecto-final-backend/models/rol.js
+++ proyecto-final-backend/models/rol.js
@@ -4,7 +4,7 @@ const {Schema} = mongoose;
 const RolSchema = new Schema({
  nombreRol: {type: String, required: true},
  verAnuncio: {type:Boolean, required: true},
- creaAnuncio: {type:Boolean, required: true},
+ creaAnuncio: {type:Boolean, required:true},
  gestiona: {type: Boolean, required: true},
  personas: [{type: mongoose.Types.ObjectId, ref: 'Persona'}]
 })
