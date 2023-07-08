64fd87c2d6dbe165be06a1af2fd9fa32e4b82787
Antonela Flores <totte_30_98@hotmail.com>
2023-07-08 12:49:11 -0300

    arreglo

--- proyecto-final-backend/models/anuncio.js
+++ proyecto-final-backend/models/anuncio.js
@@ -3,25 +3,18 @@ const {Schema} = mongoose;
 
 
 const AnuncioSchema = new Schema({
- titulo: {type: String},
- descripcion: {type: String, required: true},
-<<<<<<< HEAD
- fechaDesde: {type: String, required: true},
- fechaHasta: {type:String, required: true},
- recursos: [{type: String}],
- area:{type: Schema.Types.ObjectId, ref: 'Area', required: true},
- tipo: {type:String, required:true}
-=======
- descripcionCard: {type: String},
- fechaDesde: {type: String},
- fechaHasta: {type:String},
- recursos: [{
-    base64:{type:String},
-    type:{type: String},
-    name:{type: String}
-}],
- tipo: {type:String}
->>>>>>> origin/develop
+    titulo: {type: String},
+    descripcionCard: {type: String},
+    fechaDesde: {type: String},
+    fechaHasta: {type:String},
+    recursos: [{
+        base64:{type:String},
+        type:{type: String},
+        name:{type: String}
+    }],
+    area:{type: Schema.Types.ObjectId, ref: 'Area', required: true},
+    tipo: {type:String}
+
 })
 
 module.exports = mongoose.models.Anuncio || mongoose.model('Anuncio', AnuncioSchema);
