88ea6aa5591e141d2d43af6ad4663325b0a6a817
Antonela Flores <totte_30_98@hotmail.com>
2023-07-07 22:31:08 -0300

    avances

--- proyecto-final-frontend/src/app/components/Recursos/new-formulario/new-formulario.component.ts
+++ proyecto-final-frontend/src/app/components/Recursos/new-formulario/new-formulario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeHtml, SafeUrl } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Formulario } from 'src/app/models/formulario';
 import { Rol } from 'src/app/models/rol';
@@ -17,17 +18,27 @@ export class NewFormularioComponent implements OnInit {
   id:string;
   private sub: any;
 
+  editorConfig = {
+    suffix: '.min',
+    plugins: 'lists link image table wordcount'
+  }
+
+  files: { base64: string, safeurl: SafeUrl, id: number, type: string }[] = [];
+  Contenido = '';
+
+
   constructor(private formService:FormularioService,
               private rolService:RolService,
               private router : Router,
-              private actRouter : ActivatedRoute) {
+              private actRouter : ActivatedRoute,
+              private sanitizer: DomSanitizer) {
   
 
   this.roles = new Array<Rol>();
   this.form  = new Formulario();
   this.id    = ''; 
 
-  this.cargarRoles();
+  // this.cargarRoles();
 
   }
 
@@ -89,20 +100,29 @@ export class NewFormularioComponent implements OnInit {
   volver(){
     this.router.navigate(['recurso']);
   }
-
-  handleFileInput(event: any) {
-    const file: File = event.target.files[0];
-    const reader: FileReader = new FileReader();
   
-    reader.onload = (e: any) => {
-      const binaryData: any = e.target.result;
-      const binaryString: string = btoa(binaryData);
-  
-      // Aquí puedes realizar cualquier operación adicional con el PDF binario
-      console.log(binaryString);
-    };
-  
-    reader.readAsBinaryString(file);
+  sanitizeHtml(html: string): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(html);
   }
-  
+
+  onFileSelected(event: any) {
+    const files = event.target.files;
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const reader = new FileReader();
+      reader.onload = () => {
+        let base64 = reader.result as string;
+        const fileType = file.type;
+        this.files.push({ 'base64': base64, 'id': this.files.length + 1, 'type': file.type, 'safeurl': this.sanitizeUrl(base64) });
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  sanitizeUrl(base64: string): SafeUrl {
+    const safeUrl = this.sanitizer.bypassSecurityTrustUrl(base64);
+    return safeUrl;
+  }
+
+
 }
