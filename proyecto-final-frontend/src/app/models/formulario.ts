a4998dff3c120dc950d62d0685d06b01ab6b11f7
Antonela Flores <totte_30_98@hotmail.com>
2023-07-10 18:59:44 -0300

    Actualización

--- proyecto-final-frontend/src/app/models/formulario.ts
+++ proyecto-final-frontend/src/app/models/formulario.ts
@@ -5,7 +5,7 @@ export class Formulario {
     _id!:string;
     tipo!:string;
     descripcion!:string;
-    archivo!:string;
+    archivo: {base64:string, type:string, name:string}[]=[]
     //mostrarPara: Array<Rol> = new Array<Rol>();
     rol!:string;
     creadoPor!: Persona;
